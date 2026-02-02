3abd06c68288e06448e801cfe4622c976fab460c
dladdha <darshanladdha1998@gmail.com>
2026-02-02 14:04:09 +0530

    Some Modifications

--- Sports/src/main/java/com/example/demo/dao/EventRepo.java
+++ Sports/src/main/java/com/example/demo/dao/EventRepo.java
@@ -3,8 +3,9 @@ package com.example.demo.dao;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import com.example.demo.model.Event;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public class EventRepo {
     // This is your "In-Memory Database"
     private final Map<String, Event> storage = new ConcurrentHashMap<>();
